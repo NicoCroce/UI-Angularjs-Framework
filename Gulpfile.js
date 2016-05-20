9af538dff5976ec8f4fdea18022239fdc8cf6459
Nico Croce <ingcrocenicolas@outlook.com>
2016-05-20 18:54:48 -0300

    Se aclararon diferentes plugins.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require("gulp"),//http://gulpjs.com/
 	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
 	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
 	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-	sourcemaps = require('gulp-sourcemaps'),
-	path = require('path'),
-	plumber = require('gulp-plumber'),
+	sourcemaps = require('gulp-sourcemaps'), //Genera un mapa de referencias para los archivos. 
+	path = require('path'), //Es de Node. Concatena.
+	plumber = require('gulp-plumber'), //Control de errores. 
 	log = gutil.log;
 
 
